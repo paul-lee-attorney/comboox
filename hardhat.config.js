dd8c08ab97c7aa98bd0d924835fa74fa39834782
paul-lee-attorney <paul_lee0919@163.com>
2026-02-13 08:48:11 +0800

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,14 @@
-
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 import { defineConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-verify";
+// import "@nomicfoundation/hardhat-chai-matchers";
+import hardhatEthers from "@nomicfoundation/hardhat-ethers";
+import hardhatVerify from "@nomicfoundation/hardhat-verify";
 
 export default defineConfig({
+  plugins: [
+    hardhatEthers,
+    hardhatVerify
+  ],
   solidity: {
     version: "0.8.24",
     settings: {
@@ -14,50 +18,56 @@ export default defineConfig({
       },
     },
   },
-  defaultNetwork: "localhost",
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      blockGasLimit: 30_000_000,
-      gas: 16_000_000,
+      type: "edr-simulated",
+      chainId: 31337,
       initialDate: "2025-01-01T00:00:00Z",
     },
-    localhost: {
-      url: "http://127.0.0.1:8545",
-    },
     arbitrumSepolia: {
-      // type: "http",
+      type: "http",
       url: `https://arb-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_SEPOLIA_KEY}`,
       accounts: [process.env.SEPOLIA_PRIV_KEY_0, process.env.SEPOLIA_PRIV_KEY_1],
     },
     mainnet: {
-      // type: "http",
+      type: "http",
       url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_MAINNET_KEY}`,
       accounts: [process.env.MAINNET_PRIV_KEY_0, process.env.MAINNET_PRIV_KEY_1, process.env.MAINNET_PRIV_KEY_2],
     },
     arbitrumOne: {
-      // type: "http",
+      type: "http",
       url: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_ARB_KEY}`,
       accounts: [process.env.ARB_PRIV_KEY_0, process.env.ARB_PRIV_KEY_1, process.env.ARB_PRIV_KEY_2],
     },
   },
-  etherscan:{
-    apiKey: process.env.ETHERSCAN_API_KEY,
+  verify: {
+    etherscan: {
+      apiKey: process.env.ETHERSCAN_API_KEY,
+      // customChains: [
+      //   {
+      //     network: "arbitrumSepolia",
+      //     chainId: 421614,
+      //     urls: {
+      //       apiURL: "https://api-sepolia.arbiscan.io/api",
+      //       browserURL: "https://sepolia.arbiscan.io/",
+      //     },
+      //   },
+      //   {
+      //     network: "arbitrumOne",
+      //     chainId: 42161,
+      //     urls: {
+      //       apiURL: "https://api.arbiscan.io/api",
+      //       browserURL: "https://arbiscan.io/",
+      //     },
+      //   },
+      // ],
+    },
   },
-  // apiKey: {
-  //   arbitrumOne: process.env.ETHERSCAN_API_KEY, // Arbiscan
-  //   arbitrumSepolia: process.env.ETHERSCAN_API_KEY,
-  // },
-  // customChains: [
-  //   {
-  //     network: 'arbitrumSepolia',
-  //     chainId: 421614,
-  //     urls: {
-  //       apiURL: "https://api-sepolia.arbiscan.io/api",
-  //       browserURL: "https://sepolia.arbiscan.io/",
-  //     }
-  //   }
-  // ]
   sourcify: {
-    enabled: true
+    enabled: true,
+  },
+  blockscout: {
+    enabled: false,
   },
 });
